b28a3ba858601cd7a1aad9a3935e41004f95e5b6
Romain Dorgueil <romain@makersquad.fr>
2026-02-18 11:04:28 +0100

    chore: bump version to 0.4.0

--- src/wilco/__init__.py
+++ src/wilco/__init__.py
@@ -3,7 +3,7 @@
 from .bundler import BundleResult, BundlerNotFoundError, bundle_component
 from .registry import Component, ComponentRegistry
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "Component",
