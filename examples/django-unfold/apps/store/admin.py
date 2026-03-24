330ef32874a687a9ad97603f1e6081070ad40f6e
Romain Dorgueil <romain@makersquad.fr>
2026-03-24 09:17:46 +0100

    fix(django-unfold): use Unfold's UserAdmin and GroupAdmin for consistent styling
    Without this, the User and Group pages in the admin use Django's default
    admin classes, which break the Unfold theme. Per Unfold docs, the default
    registrations must be unregistered and re-registered with Unfold's ModelAdmin
    and custom forms (UserChangeForm, UserCreationForm, AdminPasswordChangeForm).

--- examples/django-unfold/apps/store/admin.py
+++ examples/django-unfold/apps/store/admin.py
@@ -1,12 +1,31 @@
 from typing import Any
 
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group, User
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from wilco.bridges.django import LivePreviewAdminMixin
 
 from .models import Product
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
 
 
 @admin.register(Product)
